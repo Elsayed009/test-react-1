e763ff5a93585275abf95746f50ad80b2ac70664
Elsayed009 <elsayedelshaerawy@gmail.com>
2026-03-16 13:16:12 +0200

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
 
 import './App.css'
-import { About } from './assets/About/About'
-import { Home } from './Home/Home'
-import { Test } from './Test/Test'
-import { Counter } from './Test/Counter'
-function App() {
- 
+import {About} from './Component/About/About'
+import { Home } from './Component/Home/Home'
+import { Test } from './Component/Test/Test'
+import { Counter } from './Component/Test/Counter'
+import {createBrowserRouter, RouterProvider, Outlet} from "react-router";
+import { NotFound } from './Component/NotFound/NotFound'
+import { NavBar } from './Component/NavBar/NavBar';
 
+function Layout() {
   return <>
-  {console.log("render")}
- <Home/>
- <About/>
- <Test name= "Elsayed" job="software Engineer"/>
- <Counter/>
+    <NavBar/>
+    <Outlet/>
+  </>
+}
+let rout = createBrowserRouter([
+  // {path:"/home", element: <Home/>},
+  // {path:"NavBar", element: <NavBar/>},
+  {
+
+    element: <Layout/>,
+    children: [
+      
+      // {index: true, element: <Home/>},
+      { path: "/", element: <Home/> },
+      {path: "/About", element: <About/>},
+      {path:"/Test", element:  <Test name= "Elsayed" job="software Engineer"/>},
+      {path:"/Counter", element: <Counter/>},
+      {path:"*", element: <NotFound/>}
+    ]
+  }
+])
+function App() {
+  return <>
+  {/* <NavBar/> */}
+  {/* {console.log("render")} */}
+  <RouterProvider router={rout}/>
+ 
   </>
 }
 
---
+++ src/Component/NotFound/NotFound.jsx
@@ -0,0 +1,5 @@
+export function NotFound(){
+    return<>
+    <h1>page not found</h1>
+    </>
+}
